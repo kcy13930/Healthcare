0ed5bc3ff878e2626e7876baf99560330e46e6f7
Hyung-Jeong Yang <96161431+SclabCNU@users.noreply.github.com>
2023-11-23 15:22:15 +0900

    Delete 2023 directory

--- 2023/[Seungjun]긴 맥락 대화에서의 감정인식을 위한 확장형 트랜스포머/code/loss.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torch.nn.functional as F
-
-def focal_loss(preds, targets, alpha = .25, gamma = 2):
-    CE_loss = F.cross_entropy(preds, targets, reduction='none')
-    pt = torch.exp(-CE_loss) # prevents nans when probability 0
-    F_loss = alpha * (1-pt)**gamma * CE_loss
-    return F_loss.mean()
